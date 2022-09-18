8bf8046bd4cf53cb883ccd72e1b0336bf72340f8
usernyan <nobody@example.com>
2022-09-18 06:40:25 -0400

    testing modifying list values

--- test.c
+++ test.c
@@ -8,8 +8,17 @@ void print_int(void *data);
 int main(int argc, char *argv[]) {
 	printf("Hello, test.\n");
 	printf(getfoo()); //Testing #include "linked.h"
-	struct link_node *my_list = new_list(5);
+	int l = 5;
+	struct link_node *my_list = new_list(l);
 	init_list(my_list, &init_int);
+	print_list(my_list, &print_int);
+	int i = 0;
+	struct link_node *cur_node = my_list;
+	while (cur_node) {
+		*(int *)cur_node->data = i * i + 1;
+		cur_node = cur_node->next;
+		i++;
+	}
 	print_list(my_list, &print_int);
 	return 0;
 }
