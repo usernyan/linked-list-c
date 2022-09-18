10e53cf5f1cd5781a42788a30ae18f45f223cf2f
usernyan <nobody@example.com>
2022-09-18 17:04:17 -0400

    refactor test-driver, added int_list_tester

--- test-driver.c
+++ test-driver.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include "linked.h"
 
+//these functions are passes as pointers to the library
 void init_int(void **to_data);
 void print_int(void *data);
 void free_int(void **to_data);
+
+//these are just helper functions for testing.
+void int_list_tester(struct link_node *list_head);
+
 
 int main(int argc, char *argv[]) {
 	printf("Hello, test.\n");
@@ -13,17 +18,21 @@ int main(int argc, char *argv[]) {
 	struct link_node *my_list = new_list(l);
 	init_list(my_list, &init_int);
 	print_list(my_list, &print_int);
+	int_list_tester(my_list);
+	print_list(my_list, &print_int);
+	clear_list(my_list, &free_int);
+	dealloc_nodes(my_list);
+	return 0;
+}
+
+void int_list_tester(struct link_node *list_head) {
 	int i = 0;
-	struct link_node *cur_node = my_list;
+	struct link_node *cur_node = list_head;
 	while (cur_node) {
 		*(int *)cur_node->data = i * i + 1;
 		cur_node = cur_node->next;
 		i++;
 	}
-	print_list(my_list, &print_int);
-	clear_list(my_list, &free_int);
-	dealloc_nodes(my_list);
-	return 0;
 }
 
 void init_int(void **to_data) {
